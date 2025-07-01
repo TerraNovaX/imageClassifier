9b37c7cdf52edd624b8d284deb0ea0934e7f0433
Salma WADOUACHI <wadouachisalma@gmail.com>
2025-07-01 19:26:50 +0200

    fix: render upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model.eval()
 transform = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 def predict_image(img_path):
